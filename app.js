20caeb35aa7c2d8394a81cf6e7059cc7ef0fc5d1
Shubham <shup30@gmail.com>
2019-10-05 13:47:49 +0530

    second-commit

--- app.js
+++ app.js
@@ -17,42 +17,42 @@ db.once('open', function () {
 
 //use sessions for tracking logins
 app.use(session({
-    secret: 'work hard',
-    resave: true,
-    saveUninitialized: false,
-    store: new MongoStore({
-      mongooseConnection: db
-    })
-  }));
-  
-  // parse incoming requests
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  
-  
-  // serve static files from template
-  app.use(express.static(__dirname + '/templateLogReg'));
-  
-  // include routes
-  var routes = require('./routes/router');
-  app.use('/', routes);
-  
-  // catch 404 and forward to error handler
-  app.use(function (req, res, next) {
-    var err = new Error('File Not Found');
-    err.status = 404;
-    next(err);
-  });
-  
-  // error handler
-  // define as the last app.use callback
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.send(err.message);
-  });
-  
-  
-  // listen on port 3000
-  app.listen(3000, function () {
-    console.log('Express app listening on port 3000');
-  });
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// serve static files from template
+app.use(express.static(__dirname + '/templateLogReg'));
+
+// include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+// listen on port 3000
+app.listen(3000, function () {
+  console.log('Express app listening on port 3000');
+});
